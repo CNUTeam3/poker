49e2a18fc635974f79d56b3d7b3a684839dfe7d6
Kimjinhyeok <persona5173@gmail.com>
2017-04-25 22:24:14 +0900

    Update EvaluatorTest.java

--- src/test/java/com/edu/cnu/poker/EvaluatorTest.java
+++ src/test/java/com/edu/cnu/poker/EvaluatorTest.java
@@ -12,6 +12,48 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by cse on 2017-04-17.
  */
 public class EvaluatorTest {
+    @Test
+    public void SUIT가_같은_AKQJ10은_로열스트레이트플러쉬이다() {
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(1, Suit.CLUBS),
+                new Card(10, Suit.CLUBS),
+                new Card(11, Suit.CLUBS),
+                new Card(12, Suit.CLUBS),
+                new Card(13, Suit.CLUBS)
+        );
+        String result = evaluator.evaluate(cardList);
+        assertThat(result, is("ROALSTRAIGHTFLUSH"));
+    }
+
+    @Test
+    public void 백스트레이트플러쉬() {
+
+    }
+
+    @Test
+    public void 스트레이트플러쉬() {
+
+    }
+
+    @Test
+    public void RANK가_4개가동일하면_포카드다() {
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(4, Suit.CLUBS),
+                new Card(4, Suit.DIAMONDS),
+                new Card(4, Suit.HEARTS),
+                new Card(6, Suit.HEARTS),
+                new Card(4, Suit.SPADES)
+        );
+        String result = evaluator.evaluate(cardList);
+        assertThat(result, is("FOURCARD"));
+    }
+
+    @Test
+    public void RANK가_동일한수가3개_동일한수가2개이면_FULLHOUSE이다() {
+        Evaluator evaluator = new Evaluator();
+    }
 
     @Test
     public void SUIT가_5개가동일하면_플러쉬다() {
@@ -25,64 +67,6 @@ public class EvaluatorTest {
         );
         String result = evaluator.evaluate(cardList);
         assertThat(result, is("FLUSH"));
-    }
-
-    @Test
-    public void RANK가_동일한수가3개_동일한수가2개이면_FULLHOUSE이다() {
-        Evaluator evaluator = new Evaluator();
-    }
-
-    @Test
-    public void RANK가_같은_카드가_2쌍이면_투페어다() {
-        Evaluator evaluator = new Evaluator();
-        List<Card> cardList = Arrays.asList(
-                new Card(1, Suit.SPADES),
-                new Card(1, Suit.CLUBS),
-                new Card(2, Suit.DIAMONDS),
-                new Card(2, Suit.HEARTS)
-        );
-        String result = evaluator.evaluate(cardList);
-        assertThat(result, is("TWOPAIR"));
-    }
-
-    @Test
-    public void RANK가_같은_카드가_3장이면_트리플이다() {
-        Evaluator evaluator = new Evaluator();
-        List<Card> cardList = Arrays.asList(
-                new Card(1, Suit.SPADES),
-                new Card(1, Suit.CLUBS),
-                new Card(1, Suit.HEARTS)
-        );
-        String result = evaluator.evaluate(cardList);
-        assertThat(result, is("TRIPPLE"));
-    }
-
-    @Test
-    public void RANK가_연달아_3장의_카드가_존재하면_스트레이트이다() {
-        Evaluator evaluator = new Evaluator();
-        List<Card> cardList = Arrays.asList(
-                new Card(1, Suit.SPADES),
-                new Card(2, Suit.CLUBS),
-                new Card(3, Suit.SPADES),
-                new Card(4, Suit.HEARTS),
-                new Card(5, Suit.SPADES)
-        );
-        String result = evaluator.evaluate(cardList);
-        assertThat(result, is("STRAIGHT"));
-    }
-
-    @Test
-    public void SUIT가_같은_AKQJ10은_로열스트레이트플러쉬이다() {
-        Evaluator evaluator = new Evaluator();
-        List<Card> cardList = Arrays.asList(
-                new Card(1, Suit.CLUBS),
-                new Card(10, Suit.CLUBS),
-                new Card(11, Suit.CLUBS),
-                new Card(12, Suit.CLUBS),
-                new Card(13, Suit.CLUBS)
-        );
-        String result = evaluator.evaluate(cardList);
-        assertThat(result, is("ROALSTRAIGHTFLUSH"));
     }
 
     @Test
@@ -100,17 +84,48 @@ public class EvaluatorTest {
     }
 
     @Test
-    public void RANK가_4개가동일하면_포카드다() {
+    public void 백스트레이트() {
+
+    }
+
+
+    @Test
+    public void RANK가_연달아_3장의_카드가_존재하면_스트레이트이다() {
         Evaluator evaluator = new Evaluator();
         List<Card> cardList = Arrays.asList(
-                new Card(4, Suit.CLUBS),
-                new Card(4, Suit.DIAMONDS),
-                new Card(4, Suit.HEARTS),
-                new Card(6, Suit.HEARTS),
-                new Card(4, Suit.SPADES)
+                new Card(4, Suit.SPADES),
+                new Card(5, Suit.CLUBS),
+                new Card(6, Suit.SPADES),
+                new Card(7, Suit.HEARTS),
+                new Card(8, Suit.SPADES)
         );
         String result = evaluator.evaluate(cardList);
-        assertThat(result, is("FOURCARD"));
+        assertThat(result, is("STRAIGHT"));
+    }
+
+    @Test
+    public void RANK가_같은_카드가_3장이면_트리플이다() {
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(1, Suit.SPADES),
+                new Card(1, Suit.CLUBS),
+                new Card(1, Suit.HEARTS)
+        );
+        String result = evaluator.evaluate(cardList);
+        assertThat(result, is("TRIPPLE"));
+    }
+
+    @Test
+    public void RANK가_같은_카드가_2쌍이면_투페어다() {
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(1, Suit.SPADES),
+                new Card(1, Suit.CLUBS),
+                new Card(2, Suit.DIAMONDS),
+                new Card(2, Suit.HEARTS)
+        );
+        String result = evaluator.evaluate(cardList);
+        assertThat(result, is("TWOPAIR"));
     }
 
     @Test
